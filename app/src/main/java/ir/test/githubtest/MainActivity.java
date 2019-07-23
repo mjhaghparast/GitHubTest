2d0941f4a5b125494583264272ab993b81c9e445
ALLAH <haghparast289@gmail.com>
2019-07-23 18:07:05 +0430

    haghparast 2

--- app/src/main/java/ir/test/githubtest/MainActivity.java
+++ app/src/main/java/ir/test/githubtest/MainActivity.java
@@ -1,7 +1,9 @@
 package ir.test.githubtest;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -11,5 +13,15 @@ public class MainActivity extends AppCompatActivity
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		findViewById(R.id.button).setOnClickListener(new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View view)
+			{
+				Toast.makeText(MainActivity.this, "hello", Toast.LENGTH_SHORT).show();
+			}
+		});
+
 	}
 }
