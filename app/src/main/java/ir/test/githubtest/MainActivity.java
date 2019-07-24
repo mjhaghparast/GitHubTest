b7bfcf4d2d012a8db848e8876485ab1cd3d6244e
ALLAH <haghparast289@gmail.com>
2019-07-24 10:29:21 +0430

    delete toast bottun click

--- app/src/main/java/ir/test/githubtest/MainActivity.java
+++ app/src/main/java/ir/test/githubtest/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity
 			@Override
 			public void onClick(View view)
 			{
-				Toast.makeText(MainActivity.this, "hello", Toast.LENGTH_SHORT).show();
+
 			}
 		});
 
